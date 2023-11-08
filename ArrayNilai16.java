4c0004d6a6ae0d0a83ea315727897d828eaa3264
khesyalv <145529469+khesyalv@users.noreply.github.com>
2023-11-08 15:48:04 +0700

    Add files via upload

---
+++ ArrayNilai16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayNilai16{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+        
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = scan.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70){
+                System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            }
+        }
+    }
+}
